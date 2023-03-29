e75c6098c17b04c09601b36d5e6d6989699f4896
Pravin <pravin08pawar@gmail.com>
2023-03-29 23:55:38 +0530

    Navbar done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout"
+
+
+const Footer = ()=>{
+
+    return (
+        <Box>
+            
+        </Box>
+    )
+}
+export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+import Navbar from "../components/Navbar"
+const Home = ()=>{
+
+    return (
+<Box>
+    <Navbar/>
+</Box>
+    )
+}
+
+export default Home

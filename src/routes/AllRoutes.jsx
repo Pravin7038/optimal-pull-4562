a3bdabf6875afb894565c8fabdd5378f53f7cc1f
Pravin <pravin08pawar@gmail.com>
2023-03-30 13:15:44 +0530

    footer created

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -4,20 +4,20 @@ import Home from '../pages/Home'
 export default function AllRoutes() {
     return (
         <Routes>
-            <Route path='/' element ={<Home/>}></Route>
-            <Route path='/gifts' element ={<Home/>}></Route>
-            <Route path='/women' element ={<Home/>}></Route>
-            <Route path='/men' element ={<Home/>}></Route>
-            <Route path='/kids' element ={<Home/>}></Route>
-            <Route path='/shoes' element ={<Home/>}></Route>
-            <Route path='/handbags' element ={<Home/>}></Route>
-            <Route path='/jewelry' element ={<Home/>}></Route>
-            <Route path='/beuty' element ={<Home/>}></Route>
-            <Route path='/home' element ={<Home/>}></Route>
-            <Route path='/bed&bath' element ={<Home/>}></Route>
-            <Route path="/Conn'sxBelk" element ={<Home/>}></Route>
-            <Route path='/brands' element ={<Home/>}></Route>
-            <Route path='/clearence' element ={<Home/>}></Route>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/gifts' element={<Home />}></Route>
+            <Route path='/women' element={<Home />}></Route>
+            <Route path='/men' element={<Home />}></Route>
+            <Route path='/kids' element={<Home />}></Route>
+            <Route path='/shoes' element={<Home />}></Route>
+            <Route path='/handbags' element={<Home />}></Route>
+            <Route path='/jewelry' element={<Home />}></Route>
+            <Route path='/beuty' element={<Home />}></Route>
+            <Route path='/home' element={<Home />}></Route>
+            <Route path='/bed&bath' element={<Home />}></Route>
+            <Route path="/Conn'sxBelk" element={<Home />}></Route>
+            <Route path='/brands' element={<Home />}></Route>
+            <Route path='/clearence' element={<Home />}></Route>
         </Routes>
     )
 }
